3837496ffca2fb65085a6e80bae419b1464d2506
Gaurisha21 <68791455+Gaurisha21@users.noreply.github.com>
2021-08-17 19:42:20 +0530

    initial commits

---
+++ app.py
@@ -0,0 +1,73 @@
+soothingSongs = [
+	{
+		"id": 1,
+		"SoothingSongs": "https://www.youtube.com/watch?v=h3uJKEDsyCM"
+	},
+	{
+		"id": 2,
+		"SoothingSongs": "https://www.youtube.com/watch?v=xgc1SlB9gk4"
+	},
+	{
+		"id": 3,
+		"SoothingSongs": "https://www.youtube.com/watch?v=CcsUYu0PVxY"
+	},
+	{
+		"id": 4,
+		"SoothingSongs": "https://www.youtube.com/watch?v=CcsUYu0PVxY"
+	},
+	{
+		"id": 5,
+		"SoothingSongs": "https://www.youtube.com/watch?v=lFcSrYw-ARY"
+	},
+	{
+		"id": 6,
+		"SoothingSongs": "https://www.youtube.com/watch?v=_zScs9cYWsw"
+	},
+	{
+		"id": 7,
+		"SoothingSongs": "https://www.youtube.com/watch?v=KRA26LhuTP4"
+	},
+	{
+		"id": 8,
+		"SoothingSongs": "https://www.youtube.com/watch?v=ZFxw4lfzckI"
+	},
+	{
+		"id": 9,
+		"SoothingSongs": "https://www.youtube.com/watch?v=bP9gMpl1gyQ"
+	}
+]
+
+import flask
+from flask import request, jsonify
+import random
+
+app = flask.Flask(__name__)
+# app.config["DEBUG"] = True
+
+@app.route('/', methods=['GET'])
+def home():
+    return '''<h1>Distant Reading Archive</h1>
+<p>A prototype API for distant reading of science fiction novels.</p>'''
+
+
+@app.route('/api/songs/all', methods=['GET'])
+def api_all():
+    return jsonify(soothingSongs)
+
+
+@app.route('/api/songs/random', methods=['GET'])
+def api_id():
+    results = []
+    r = random.randint(1,9)
+    # r0 = str(r)
+    # Loop through the data and match results that fit the requested ID.
+    # IDs are unique, but other fields might return many results
+    for song in soothingSongs:
+        if song['id'] == r:
+            results.append(song)
+
+    # Use the jsonify function from Flask to convert our list of
+    # Python dictionaries to the JSON format.
+    return jsonify(results)
+if __name__ == "__main__":
+    app.run(debug = True, use_reloader=False)
